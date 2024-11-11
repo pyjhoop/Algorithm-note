f5f2d4c2d2368b5fae564ad167cfa5efdfbe681c
pyjhoop <pyjhoop@naver.com>
2024-11-11 19:05:14 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 96.84 ms, Memory: 96.4 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,26 +1,6 @@
-import java.util.*;
 class Solution {
-    static HashMap<String,Integer> map = new HashMap<>();
     public String solution(String[] participant, String[] completion) {
-        for(String str: participant) {
-           map.put(str, map.getOrDefault(str, 0) + 1);
-        }
-        return getIncompleName(completion, participant);
-    }
-    
-    static String getIncompleName(String[] completion, String[] participant) {
         String answer = "";
-        
-         for (String str : completion) {
-            map.put(str, map.get(str) - 1);
-        }
-
-        for (String str : participant) {
-            if (map.get(str) == 1) {
-               answer = str;
-            }
-        }
-        
         return answer;
     }
 }
