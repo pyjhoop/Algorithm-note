b989b1501a01ccbc633ca2b6db9f01269b844684
pyjhoop <pyjhoop@naver.com>
2024-11-16 21:39:57 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 20.42 ms, Memory: 94.8 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
@@ -4,19 +4,19 @@ class Solution {
     public int solution(String[] friends, String[] gifts) {
         HashMap<String, Integer> map = new HashMap<>();
         int[][] arr = new int[friends.length][friends.length];
-        int[] giftsIndex = new int[friends.length];
-        int[] giftsResult = new int[friends.length];
+        int[] giftsIndex = new int[friends.length]; // 선물지수
+        int[] giftsResult = new int[friends.length]; // 받을 선물
         // 1. 맵 초기화
         for(int i = 0; i < friends.length; i++){
             map.put(friends[i], i);
         }
-        // 2. 이중 배열초기화 - gifts이용해서
+        // 2. 선물 관계와 지수 계산
         for(String str: gifts) {
             String[] names = str.split(" ");
             int senderIndex = map.get(names[0]);
             int receiverIndex = map.get(names[1]);
             arr[senderIndex][receiverIndex]++;
-            // 여기서 선물지수 초기화해야함.
+            
             giftsIndex[senderIndex]++;
             giftsIndex[receiverIndex]--;
         }
@@ -27,9 +27,8 @@ class Solution {
         
         for(int i = 0; i < arr.length; i++) {
             for(int j = 0; j < arr[i].length; j++) {
-                // 나자신은 무시
                 if(j == i) continue;
-                // 먼저 0,1에서 선물 받은 개수 구해보자
+                
                 int iCount = arr[i][j];
                 int jCount = arr[j][i];
                 
@@ -37,26 +36,16 @@ class Solution {
                     giftsResult[i]++;
                 }else if(jCount > iCount) {
                     giftsResult[j]++;
-                }else if(jCount == iCount) {
-                    // 지수를 구해야함.
-                    int iIndex = giftsIndex[i];
-                    int jIndex = giftsIndex[j];
-                    if(iIndex > jIndex) {
-                        giftsResult[i]++;
-                    }else if(jIndex > iIndex){
-                        giftsResult[j]++;
-                    }
+                } else if (giftsIndex[i] > giftsIndex[j]) {
+                    giftsResult[i]++;
+                } else if (giftsIndex[j] > giftsIndex[i]) {
+                    giftsResult[j]++;
                 }
                 
             }
         }
         
-        // giftsResult 반복문 돌려서 최대값을 찾아야함.
-        int max = 0;
-        for(int i: giftsResult){
-            max = Math.max(max, i);
-        }
-        
-        return max/2;
+       // 최대 선물 수 찾기
+        return Arrays.stream(giftsResult).max().orElse(0) / 2;
     }
 }
