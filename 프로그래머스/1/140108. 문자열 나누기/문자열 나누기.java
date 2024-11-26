cb011878eaa4d5f9b6f6571b5e27ca5d8e976afc
pyjhoop <pyjhoop@naver.com>
2024-11-26 19:39:31 +0900

    [level 1] Title: 문자열 나누기, Time: 10.52 ms, Memory: 80.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        String[] arr = s.split("");
+        String initX = "";
+        int x = 0;
+        int y = 0;
+        
+        for(int i = 0; i < arr.length; i++) {
+            if(x == 0){
+                initX = arr[i];
+                x++;
+                continue;
+            }
+            
+            if(!arr[i].equals(initX)){
+                y++;
+                if(x == y) {
+                    answer++;
+                    initX = "";
+                    x = 0;
+                    y = 0;
+                }
+            }else{
+                x++;
+            }
+        }
+        
+        if(x > 0 || y > 0) answer++;
+        
+        
+        
+        return answer;
+    }
+}
