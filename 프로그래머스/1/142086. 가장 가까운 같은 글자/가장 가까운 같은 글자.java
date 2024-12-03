37fc4656dd2664a3e0a897edc0f181af0bb9426f
pyjhoop <pyjhoop@naver.com>
2024-12-03 22:50:01 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 13.98 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -2,24 +2,12 @@ import java.util.*;
 class Solution {
     public int[] solution(String s) {
         int[] arr = new int[s.length()];
-        Map<String, Integer> map = new HashMap<>(){
-            {
-                for(int i = 'a'; i<= 'z'; i++) {
-                    put(String.valueOf((char)i), -1);
-                }
-            }
-        };
-
+        Map<String, Integer> map = new HashMap<>();
         String[] stringArr = s.split("");
 
         for(int i = 0; i < stringArr.length; i++){
-            if(map.get(stringArr[i]) == -1){
-                map.put(stringArr[i], i);
-                arr[i] = -1;
-            }else {
-                arr[i] = i - map.get(stringArr[i]);
-                map.put(stringArr[i], i);
-            }
+            arr[i] = i-map.getOrDefault(stringArr[i],i+1);
+            map.put(stringArr[i], i);
         }
         return arr;
     }
