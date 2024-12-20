95b882e13555ee8896dd11aec30ffd07c16f2c64
pyjhoop <pyjhoop@naver.com>
2024-12-20 22:16:16 +0900

    [level 1] Title: 콜라 문제, Time: 0.09 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -1,15 +1,12 @@
 class Solution {
     public int solution(int a, int b, int n) {
         int answer = 0;
-        // n: 빈병, a: 제출 빈병 b: 반환 콜라
-        // 몫, 나머지 
-        // 20 -> 10 > 5 > 2,1(3) > 1,1(2) > 1
-        
+        // 반환받는 총 콜라수, 빈병 구하고
+        // 반환받는 총 콜라수 또한 빈병이 될 예정이기에 n을 반환받은 콜라수 + 빈병으로 설정 후 n이 a-1 초과 조건동안 반복문 
+
         while(n > a - 1) {
-            int cok = (n / a) * b;
-            int restBlank = n % a;
-            answer += cok;
-            n = restBlank + cok;
+            answer += (n / a) * b;
+            n = n % a + ((n / a) * b);
         }
         
         return answer;
