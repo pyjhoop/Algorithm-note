2b8c9ae635992d669b44a434e0097e7c1997e42c
pyjhoop <pyjhoop@naver.com>
2025-01-13 21:46:01 +0900

    [Silver IV] Title: 암기왕, Time: 1672 ms, Memory: 227696 KB -BaekjoonHub

--- 백준/Silver/2776. 암기왕/암기왕.java
+++ 백준/Silver/2776. 암기왕/암기왕.java
@@ -1,32 +1,58 @@
 import java.io.*;
-import java.util.HashMap;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
 
         int T = Integer.parseInt(br.readLine());
 
         for(int i = 0; i < T; i++) {
             int note1Length = Integer.parseInt(br.readLine());
-            String[] note1Arr = br.readLine().split(" ");
-            HashMap<String, Integer> map = new HashMap<>();
-
+            st = new StringTokenizer(br.readLine());
+            int[] note1Arr = new int[note1Length];
             for(int j = 0; j < note1Length; j++) {
-                map.putIfAbsent(note1Arr[j], j);
+                note1Arr[j] = Integer.parseInt(st.nextToken());
             }
+            Arrays.sort(note1Arr);
 
             int note2Length = Integer.parseInt(br.readLine());
-            String[] note2Arr = br.readLine().split(" ");
+            int[] note2Arr = new int[note2Length];
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < note2Length; j++) {
+                note2Arr[j] = Integer.parseInt(st.nextToken());
+            }
 
-            for(String note2Num : note2Arr) {
-                if(map.get(note2Num) != null){
+            for(int note2: note2Arr) {
+                int start = 0;
+                int end = note1Length - 1;
+                int mid = end / 2;
+
+                boolean flag = false;
+                while(start <= end) {
+                    if(note1Arr[mid] > note2){
+                        end = mid - 1;
+                        mid = (start + end) / 2;
+                    }else if(note1Arr[mid] < note2) {
+                        start = mid + 1;
+                        mid = (start + end) / 2;
+                    }else {
+                        flag = true;
+                        break;
+                    }
+                }
+
+                if(flag) {
                     bw.write("1\n");
                 }else {
                     bw.write("0\n");
                 }
             }
+
         }
 
         bw.flush();
