da1db806b28950b1cda1fd8a2c5d46c86098ac30
pyjhoop <pyjhoop@naver.com>
2025-01-22 20:27:51 +0900

    [Silver I] Title: 단지번호붙이기, Time: 180 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+public class Main {
+    static int n;
+    static int[][] map;
+    static boolean[][] visited;
+    static int[] dx = {-1, 1, 0, 0}; // 상하좌우 이동
+    static int[] dy = {0, 0, -1, 1};
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        n = scanner.nextInt();
+        map = new int[n][n];
+        visited = new boolean[n][n];
+
+        for (int i = 0; i < n; i++) {
+            String line = scanner.next();
+            for (int j = 0; j < n; j++) {
+                map[i][j] = line.charAt(j) - '0';
+            }
+        }
+
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!visited[i][j] && map[i][j] == 1) {
+                    result.add(dfs(i, j));
+                }
+            }
+        }
+
+        Collections.sort(result);
+        System.out.println(result.size());
+        for (int size : result) {
+            System.out.println(size);
+        }
+    }
+
+    static int dfs(int x, int y) {
+        visited[x][y] = true;
+        int count = 1;
+
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx >= 0 && ny >= 0 && nx < n && ny < n) {
+                if (!visited[nx][ny] && map[nx][ny] == 1) {
+                    count += dfs(nx, ny);
+                }
+            }
+        }
+
+        return count;
+    }
+}
